d67cd1aba85073e8c408c6fadf2f24afa2c31f5f
oktamov <mr006developer@gmail.com>
2024-02-28 18:53:28 +0500

    initial commit

--- root/settings.py
+++ root/settings.py
@@ -139,6 +139,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {"Basic": {"type": "basic"}},
+    "USE_SESSION_AUTH": True,
+    "DEEP_LINKING": True,
+}
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
+}
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
