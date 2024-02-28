f7e9e3072f768ccb71826b2c1f4486377bc0b89a
oktamov <mr006developer@gmail.com>
2024-02-28 20:56:40 +0500

    initial commit

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import UserForm1, UserForm2
 
-admin.site.register(UserForm1)
-admin.site.register(UserForm2)
+# UserForm1 uchun mukammal admin paneli
+class UserForm1Admin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone', 'email', 'nationality', 'date', 'address', 'amount')
+    search_fields = ('full_name', 'email', 'nationality')
+    list_filter = ('nationality', 'date')
+    date_hierarchy = 'date'
+
+# UserForm2 uchun mukammal admin paneli
+class UserForm2Admin(admin.ModelAdmin):
+    list_display = ('full_name', 'role', 'email')
+    search_fields = ('full_name', 'email')
+    list_filter = ('role',)
+
+# Modellarni admin panelida ro'yxatdan o'tkazish
+admin.site.register(UserForm1, UserForm1Admin)
+admin.site.register(UserForm2, UserForm2Admin)
