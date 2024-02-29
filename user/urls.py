4cb6fe982036f9d817216724772dc3a93f1f84f3
oktamov <mr006developer@gmail.com>
2024-02-29 17:35:57 +0500

    initial commit

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import UserForm1CreateAPIView, UserForm2CreateAPIView, SendEmailAPIView
+from .views import UserForm1CreateAPIView, UserForm2CreateAPIView
 
 urlpatterns = [
     path('userform1/create/', UserForm1CreateAPIView.as_view(), name='userform1-create'),
     path('userform2/create/', UserForm2CreateAPIView.as_view(), name='userform2-create'),
-    path('send-email/', SendEmailAPIView.as_view(), name='send-email'),
+    # path('send-email/', SendEmailAPIView.as_view(), name='send-email'),
 
 ]
 
--- user/views.py
+++ user/views.py
@@ -6,6 +6,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from root import settings
 from .models import UserForm1, UserForm2
 from .serializer import UserForm1Serializer, UserForm2Serializer, EmailSerializer
 
@@ -14,25 +15,57 @@ class UserForm1CreateAPIView(generics.ListCreateAPIView):
     queryset = UserForm1.objects.all()
     serializer_class = UserForm1Serializer
 
-
-class UserForm2CreateAPIView(generics.ListCreateAPIView):
-    queryset = UserForm2.objects.all()
-    serializer_class = UserForm2Serializer
-
-
-class SendEmailAPIView(APIView):
-    @swagger_auto_schema(request_body=EmailSerializer)
-    def post(self, request, *args, **kwargs):
-        serializer = EmailSerializer(data=request.data)
-        if serializer.is_valid():
-            email = serializer.validated_data['email']
-            text = serializer.validated_data['text']
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        email = instance.email
+        text = 'Sizning ma\'lumotingiz qabul qilindi!'
+        try:
             send_mail(
-                subject='Message from Your Django App',
+                subject='Ma\'lumot qabuli',
                 message=text,
                 from_email=os.getenv("EMAIL_HOST_USER"),
                 recipient_list=[email],
                 fail_silently=False,
             )
             return Response({'message': 'Email sent successfully'}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': 'Email don\'t send successfully'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserForm2CreateAPIView(generics.ListCreateAPIView):
+    queryset = UserForm2.objects.all()
+    serializer_class = UserForm2Serializer
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        email = instance.email
+        text = 'Sizning ma\'lumotingiz qabul qilindi!'
+        try:
+            send_mail(
+                subject='Ma\'lumot qabuli',
+                message=text,
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[email],
+                fail_silently=False,
+            )
+            return Response({'message': 'Email sent successfully'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': 'Email don\'t send successfully'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class SendEmailAPIView(APIView):
+#     @swagger_auto_schema(request_body=EmailSerializer)
+#     def post(self, request, *args, **kwargs):
+#         serializer = EmailSerializer(data=request.data)
+#         if serializer.is_valid():
+#             email = serializer.validated_data['email']
+#             text = serializer.validated_data['text']
+#             send_mail(
+#                 subject='Message from Your Django App',
+#                 message=text,
+#                 from_email=os.getenv("EMAIL_HOST_USER"),
+#                 recipient_list=[email],
+#                 fail_silently=False,
+#             )
+#             return Response({'message': 'Email sent successfully'}, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
